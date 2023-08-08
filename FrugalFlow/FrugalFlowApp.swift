b21933b11cd1df7efafa70c13c7af71f9d130ed2
Harsha Amarasinghe <gm.harsha.amarasinghe@gmail.com>
2023-08-08 17:10:38 +0530

    Initial Commit

---
+++ FrugalFlow/FrugalFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrugalFlowApp.swift
+//  FrugalFlow
+//
+//  Created by Harsha Amarasinghe on 2023-08-08.
+//
+
+import SwiftUI
+
+@main
+struct FrugalFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
